dd303e123fed0bf46a7985936083bba27d6c2a9c
Vinayak Sudhakar Kone <vinayak-k24@users.noreply.github.com>
2024-01-28 18:41:25 +0530

    28 Jan Done

--- 01_2024 January/27_01_2024.cpp
+++ 01_2024 January/27_01_2024.cpp
@@ -6,24 +6,30 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-
-class Solution {
+class Solution
+{
 public:
-    std::string matrixChainOrder(int p[], int n) {
+    std::string matrixChainOrder(int p[], int n)
+    {
         std::vector<std::vector<int>> m(n, std::vector<int>(n, 0));
         std::vector<std::vector<int>> s(n, std::vector<int>(n, 0));
         parenthesize(m, s, p, n);
         return printParentheses(s, 1, n - 1);
     }
 
-    void parenthesize(std::vector<std::vector<int>>& m, std::vector<std::vector<int>>& s, int p[], int n) {
-        for (int d = 1; d < n - 1; d++) {
-            for (int i = 1; i < n - d; i++) {
+    void parenthesize(std::vector<std::vector<int>> &m, std::vector<std::vector<int>> &s, int p[], int n)
+    {
+        for (int d = 1; d < n - 1; d++)
+        {
+            for (int i = 1; i < n - d; i++)
+            {
                 int j = i + d;
                 m[i][j] = INT_MAX / 2; // Initialize to avoid overflow
-                for (int k = i; k < j; k++) {
+                for (int k = i; k < j; k++)
+                {
                     int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                    if (q < m[i][j]) {
+                    if (q < m[i][j])
+                    {
                         m[i][j] = q;
                         s[i][j] = k;
                     }
@@ -32,88 +38,94 @@ public:
         }
     }
 
-    std::string printParentheses(std::vector<std::vector<int>>& s, int i, int j) {
-        if (i == j) {
+    std::string printParentheses(std::vector<std::vector<int>> &s, int i, int j)
+    {
+        if (i == j)
+        {
             return std::string(1, 'A' + i - 1);
-        } else {
+        }
+        else
+        {
             return "(" + printParentheses(s, i, s[i][j]) + printParentheses(s, s[i][j] + 1, j) + ")";
         }
     }
 };
 
-
-
-
 //{ Driver Code Starts.
 
-int get(int p[],int n)
+int get(int p[], int n)
 {
-    int m[n][n],ans = 1e9; 
+    int m[n][n], ans = 1e9;
     for (int i = 1; i < n; i++)
-        m[i][i] = 0; 
-    for (int L = 2; L < n; L++) {
-        for (int i = 1; i < n-L+1; i++){ 
-            m[i][i + L - 1] = INT_MAX; 
-            for (int k = i; k <= i + L - 2; k++){ 
-                int q = m[i][k] + m[k + 1][i + L - 1] + p[i-1] * p[k] * p[i + L - 1]; 
+        m[i][i] = 0;
+    for (int L = 2; L < n; L++)
+    {
+        for (int i = 1; i < n - L + 1; i++)
+        {
+            m[i][i + L - 1] = INT_MAX;
+            for (int k = i; k <= i + L - 2; k++)
+            {
+                int q = m[i][k] + m[k + 1][i + L - 1] + p[i - 1] * p[k] * p[i + L - 1];
                 if (q < m[i][i + L - 1])
-                { 
+                {
                     m[i][i + L - 1] = q;
                 }
             }
         }
     }
-    return m[1][n-1];
+    return m[1][n - 1];
 }
 
-int find(string s,int p[])
+int find(string s, int p[])
 {
-    vector<pair<int,int>> arr;
+    vector<pair<int, int>> arr;
     int ans = 0;
-    for(auto t:s)
+    for (auto t : s)
     {
-        if(t=='(')
+        if (t == '(')
         {
-            arr.push_back({-1,-1});
+            arr.push_back({-1, -1});
         }
-        else if(t==')')
+        else if (t == ')')
         {
-            pair<int,int> b=arr.back();
+            pair<int, int> b = arr.back();
             arr.pop_back();
-            pair<int,int> a=arr.back();
+            pair<int, int> a = arr.back();
             arr.pop_back();
             arr.pop_back();
-            arr.push_back({a.first,b.second});
-            ans += a.first*a.second*b.second;
+            arr.push_back({a.first, b.second});
+            ans += a.first * a.second * b.second;
         }
-        else 
+        else
         {
-            arr.push_back({p[int(t-'A')],p[int(t-'A')+1]});
+            arr.push_back({p[int(t - 'A')], p[int(t - 'A') + 1]});
         }
     }
     return ans;
 }
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n;
-        cin>>n;
+        cin >> n;
         int p[n];
-        for(int i = 0;i < n;i++)
+        for (int i = 0; i < n; i++)
         {
-            cin>>p[i];
+            cin >> p[i];
         }
         Solution ob;
         string result = ob.matrixChainOrder(p, n);
-        if(find(result,p)==get(p,n))
+        if (find(result, p) == get(p, n))
         {
-            cout<<"True"<<endl;
+            cout << "True" << endl;
         }
         else
         {
-            cout<<"False"<<endl;
+            cout << "False" << endl;
         }
     }
     return 0;
--- 01_2024 January/28_01_2024.cpp
+++ 01_2024 January/28_01_2024.cpp
@@ -1,89 +1,97 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 // } Driver Code Ends
 
-class Solution {
+class Solution
+{
 public:
-    long long findNthNumber(long long n, long long k) {
-        auto calculateNCR = [](long long n, long long r) -> long long {
+    long long findNthNumber(long long n, long long k)
+    {
+        auto calculateNCR = [](long long n, long long r) -> long long
+        {
             long long ans = 1;
-            
+
             // n! / ((n - r)! * r!)
-            
-            for(long long i = n; i > max(r, n - r); i--)
+
+            for (long long i = n; i > max(r, n - r); i--)
                 ans *= i;
-                
+
             long long den = 1;
-            
-            for(long long i = 1; i <= min(r, n - r); i++)
+
+            for (long long i = 1; i <= min(r, n - r); i++)
                 den *= i;
-                
+
             ans /= den;
-            
+
             return ans;
         };
-        
-        vector<vector<long long>> dp(63, vector<long long> (k + 1, 0));
-        
-        for(int i = 0; i < 63; i++){
-            for(int j = 0; j < k + 1; j++){
-                for(int x = 0; x <= min(i + 1, j); x++)
+
+        vector<vector<long long>> dp(63, vector<long long>(k + 1, 0));
+
+        for (int i = 0; i < 63; i++)
+        {
+            for (int j = 0; j < k + 1; j++)
+            {
+                for (int x = 0; x <= min(i + 1, j); x++)
                     dp[i][j] += calculateNCR(i + 1, x);
             }
         }
-        
+
         long long position = 0;
-        
-        for(int i = 0; i < 63; i++){
-            if(dp[i][k] >= n){
+
+        for (int i = 0; i < 63; i++)
+        {
+            if (dp[i][k] >= n)
+            {
                 position = i;
                 break;
             }
         }
-        
+
         long long result = 0;
-        while(position > -1 and n > 0){
-            if(position + 1 <= k){
+        while (position > -1 and n > 0)
+        {
+            if (position + 1 <= k)
+            {
                 result += n - 1;
                 break;
             }
-            
-            if(position > 0 and dp[position - 1][k] < n){
+
+            if (position > 0 and dp[position - 1][k] < n)
+            {
                 result |= (1LL << position);
                 n -= dp[position - 1][k];
                 k = max(k - 1, 0LL);
             }
-            
+
             --position;
         }
-        
+
         return result;
     }
 };
 
-
 //{ Driver Code Starts.
 
-int main(){
+int main()
+{
     int t;
-    scanf("%d ",&t);
-    while(t--){
-        
+    scanf("%d ", &t);
+    while (t--)
+    {
+
         int n;
-        scanf("%d",&n);
-        
-        
+        scanf("%d", &n);
+
         int k;
-        scanf("%d",&k);
-        
+        scanf("%d", &k);
+
         Solution obj;
         long long res = obj.findNthNumber(n, k);
-        
-        cout<<res<<endl;
-        
+
+        cout << res << endl;
     }
 }
 
